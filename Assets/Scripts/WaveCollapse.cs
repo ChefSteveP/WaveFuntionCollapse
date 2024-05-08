5df3e205b39545bd56bc4b092bc3a5c92e6bb805
davidjihwan <copyeditorred@gmail.com>
2024-05-07 23:52:32 -0400

    Search function working

--- Assets/Scripts/WaveCollapse.cs
+++ Assets/Scripts/WaveCollapse.cs
@@ -1,10 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using static UnityEditor.Progress;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+using TMPro;
 
 public class WaveCollapse : MonoBehaviour
 {
+    [SerializeField]
+    private Vector2Int startTile;
+    [SerializeField]
+    private Vector2Int goalTile;
+    [SerializeField]
+    private TextMeshProUGUI statusText;
+    [SerializeField]
+    private GameObject nextLevelButton;
+
     Tilemap tilemap;
     BoundsInt bounds;
     TileBase[] allTiles;
@@ -23,6 +37,7 @@ public class WaveCollapse : MonoBehaviour
         foreach(TileBase tile in fillTiles) {
             allOptions.Add(tile.name, tile);
         }
+        nextLevelButton.SetActive(false);
     }
     void Start() { // Start is called before the first frame update
         InitializeTileOptions(); //fill the edgemask and options buffer
@@ -33,7 +48,7 @@ public class WaveCollapse : MonoBehaviour
     public void Propagate(){
         var updatedOptionsBuffer = new Dictionary<Vector2Int, Dictionary<string, TileBase>>();
 
-        //loop over the options buffer (empty tiles)
+        //loop over the options buffer (empty tiles) 
         foreach(var entry in optionsBuffer){
             var options = new Dictionary<string,TileBase>(entry.Value);
 
@@ -103,7 +118,7 @@ public class WaveCollapse : MonoBehaviour
             Dictionary<string, TileBase> options = minEntry.Value;
             if(options.Count != 0){ //get a random option
                 string[] keys = new List<string>(options.Keys).ToArray();
-                string randomKey = keys[Random.Range(0, keys.Length)];
+                string randomKey = keys[UnityEngine.Random.Range(0, keys.Length)];
                 tilemap.SetTile(new Vector3Int(minEntry.Key[0] + bounds.xMin, minEntry.Key[1] + bounds.yMin, 0), options[randomKey]);
             } 
             else { //if there are no options, set a grass tile
@@ -115,6 +130,8 @@ public class WaveCollapse : MonoBehaviour
             allTiles = tilemap.GetTilesBlock(bounds);
             optionsBuffer.Remove(minEntry.Key);
         }
+
+        ProcessState();
     }
 
     public void ClearBoard(){
@@ -129,6 +146,59 @@ public class WaveCollapse : MonoBehaviour
         optionsBuffer = new Dictionary<Vector2Int, Dictionary<string, TileBase>>();
         InitializeTileOptions(); // refill the options buffer
         Propagate();    // reduce the options
+    }
+
+    public bool SearchPath()
+    {
+        // perform BFS to check if path exists from start to goal
+        int xbounds = bounds.size.x;
+        int ybounds = bounds.size.y;
+        Queue<Vector2Int> queue = new Queue<Vector2Int> ();
+        bool[] visited = new bool[xbounds*ybounds];
+        visited[startTile.x + startTile.y * xbounds] = true;
+        queue.Enqueue(startTile);
+        // tilemap.SetTile(new Vector3Int(startTile.x + bounds.xMin, startTile.y + bounds.yMin, 0), null);
+        Debug.Log("Starting BFS");
+        while(queue.Count > 0)
+        {
+            Vector2Int currentTile = queue.Dequeue();
+            // tilemap.SetTile(new Vector3Int(currentTile.x + bounds.xMin, currentTile.y + bounds.yMin, 0), null);
+            int currx = currentTile.x;
+            int curry = currentTile.y;
+            // Debug.Log("x: " + currentTile.x.ToString() + "y: " + currentTile.y.ToString());
+            if (currentTile == goalTile) return true;
+            foreach (Vector2Int dir in new[] { new Vector2Int(0,1), new Vector2Int(1,0), new Vector2Int(0,-1), new Vector2Int(-1,0) })
+            {
+                // check if out of bounds
+                if (currx + dir.x < 0 || currx + dir.x >= xbounds
+                    || curry + dir.y < 0 || curry + dir.y >= ybounds) continue;
+                Vector2Int neighborTile = new Vector2Int(currx + dir.x, curry + dir.y);
+                if (IsConnected(currentTile, neighborTile, dir))
+                {
+                    if (!visited[neighborTile.x + neighborTile.y * xbounds])
+                    {
+                        visited[neighborTile.x + neighborTile.y * xbounds] = true;
+                        queue.Enqueue(neighborTile);
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void ProcessState()
+    {
+        if (SearchPath())
+        {
+            // win
+            statusText.text = "You Win!";
+            nextLevelButton.SetActive(true);
+        } else
+        {
+            // lose
+            statusText.text = "Try again";
+        }
     }
 
 ////////////////////////////////////////////////////////////////////////
@@ -167,5 +237,41 @@ public class WaveCollapse : MonoBehaviour
             key = allTiles[nx + ny * bounds.size.x].name[(our_idx + 2) % 4];
         }
         return (key, our_idx);
+    }
+
+    // checks if currentTile and neighborTile are connected
+    private bool IsConnected(Vector2Int currentTile, Vector2Int neighborTile, Vector2Int dir)
+    {
+        char port1, port2;
+        int port1idx = -1, port2idx = -1;
+        switch (dir.x, dir.y)
+        {   
+            case (0, 1):
+                port1idx = 0;
+                break;
+            case (1, 0):
+                port1idx = 1;
+                break;
+            case (0, -1):
+                port1idx = 2;
+                break;
+            case (-1, 0):
+                port1idx = 3;
+                break;
+            default:
+                Console.WriteLine("This should never run.");
+                break;
+        }
+        port2idx = (port1idx + 2) % 4;
+        port1 = allTiles[currentTile.x + currentTile.y * bounds.size.x].name[port1idx];
+        port2 = allTiles[neighborTile.x + neighborTile.y * bounds.size.x].name[port2idx];
+        Debug.Log("Current tile: " + allTiles[currentTile.x + currentTile.y * bounds.size.x].name);
+        Debug.Log("x: " + currentTile.x + ", y: " + currentTile.y);
+        Debug.Log("Neighbor tile: " + allTiles[neighborTile.x + neighborTile.y * bounds.size.x].name);
+        Debug.Log("x: " + neighborTile.x + ", y: " + neighborTile.y);
+        Debug.Log("Port1: " + port1 + ", port 2: " + port2);
+        bool connected = (port1 == port2) && (port1 == '1') ? true : false;
+        Debug.Log("Connected: " + connected);
+        return connected;
     }
 }
